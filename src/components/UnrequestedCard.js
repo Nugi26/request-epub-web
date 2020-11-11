92ff90b25d982b4cef4df5a70b439c2973bfee3e
adinugraha2611 <m.adinugraha.ug@gmail.com>
2020-11-11 18:33:09 +0700

    added description collapsable

--- src/components/UnrequestedCard.js
+++ src/components/UnrequestedCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Collapse from '@material-ui/core/Collapse';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import clsx from 'clsx';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -19,9 +23,24 @@ const useStyles = makeStyles(theme => ({
   cardContent: {
     flexGrow: 1,
   },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
 }));
 
 const UnrequestedCard = ({ book }) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -45,10 +64,28 @@ const UnrequestedCard = ({ book }) => {
         </Typography>
       </CardContent>
       <CardActions>
+        {/* TODO: setState for toggle */}
         <Button size="medium" color="primary">
           Tambah Request
         </Button>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="body2">
+            {book.description || 'Tidak ada deskripsi'}
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
